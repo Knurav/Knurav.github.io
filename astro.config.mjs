cffb581ed77e5a5a25084bebcc57e483fb37b527
Knurav <62768727+Knurav@users.noreply.github.com>
2022-08-22 00:52:36 +0530

    Adding base to asto config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://knurav.github.io/',
+	base: '/PersonalBlog',
 	integrations: [mdx(), sitemap()],
 });
